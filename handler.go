09e31c73905ce847529572c7896ed47d812aa79a
Andrey Trofimenko <a.trofimenko@robo.cash>
2025-03-10 17:33:07 +0300

    Fix pprof

--- handler.go
+++ handler.go
@@ -1,69 +1,69 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
 	"net/http"
-	"net/http/pprof"
-	_ "net/http/pprof"
+
+	//_ "net/http/pprof"
 	"strconv"
 	"strings"
 )
 
 func startServer() error {
 	strPort := strconv.FormatInt(cfg.Port, 10)
-	strPPPort := strconv.FormatInt(cfg.PprofPort, 10)
-
-	var tlsConf *tls.Config
-	if cfg.Crt != "" && cfg.Key != "" {
-		crt, err := tls.LoadX509KeyPair(cfg.Crt, cfg.Key)
-		if err != nil {
-			return err
-		}
-
-		tlsConf = &tls.Config{
-			Certificates: []tls.Certificate{crt},
-		}
-	}
-
 	/*
-		sockR := http.NewServeMux()
-		sockR.HandleFunc("/", handleConnections)
+		strPPPort := strconv.FormatInt(cfg.PprofPort, 10)
 
-		socServer := &http.Server{
-			Addr:      ":" + strPort,
-			TLSConfig: tlsConf,
-			Handler:   sockR,
+		var tlsConf *tls.Config
+		if cfg.Crt != "" && cfg.Key != "" {
+			crt, err := tls.LoadX509KeyPair(cfg.Crt, cfg.Key)
+			if err != nil {
+				return err
+			}
+
+			tlsConf = &tls.Config{
+				Certificates: []tls.Certificate{crt},
+			}
 		}
+
+		/*
+			sockR := http.NewServeMux()
+			sockR.HandleFunc("/", handleConnections)
+
+			socServer := &http.Server{
+				Addr:      ":" + strPort,
+				TLSConfig: tlsConf,
+				Handler:   sockR,
+			}
+
+		r := http.NewServeMux()
+		// Регистрация pprof-обработчиков
+		r.HandleFunc("/debug/pprof/", pprof.Index)
+		r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+		pprofServer := &http.Server{
+			Addr:      ":" + strPPPort,
+			TLSConfig: tlsConf,
+			Handler:   r,
+		}
+
+		defer func() {
+			//socServer.Close()
+			pprofServer.Close()
+		}()
+
+		go func() {
+			if err := pprofServer.ListenAndServe(); err != nil {
+				log.Fatal(err)
+			}
+		}()
+
+		log.Println("pprof http server started on " + strPPPort)
 
 	*/
-	r := http.NewServeMux()
-	// Регистрация pprof-обработчиков
-	r.HandleFunc("/debug/pprof/", pprof.Index)
-	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
-
-	pprofServer := &http.Server{
-		Addr:      ":" + strPPPort,
-		TLSConfig: tlsConf,
-		Handler:   r,
-	}
-
-	defer func() {
-		//socServer.Close()
-		pprofServer.Close()
-	}()
-
-	go func() {
-		if err := pprofServer.ListenAndServe(); err != nil {
-			log.Fatal(err)
-		}
-	}()
-
-	log.Println("pprof http server started on " + strPPPort)
-
 	log.Println("http server started on " + strPort)
 
 	http.HandleFunc("/", handleConnections)
