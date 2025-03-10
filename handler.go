19b28d5a1843203b7a9456c3f53c01fc6cced8a9
Andrey Trofimenko <a.trofimenko@robo.cash>
2025-03-10 16:15:32 +0300

    Return simple server

--- handler.go
+++ handler.go
@@ -1,97 +1,93 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
 	"net/http"
-	"net/http/pprof"
 	_ "net/http/pprof"
-	"os"
-	"os/signal"
 	"strconv"
 	"strings"
-	"syscall"
 )
 
 func startServer() error {
 	strPort := strconv.FormatInt(cfg.Port, 10)
-	strPPPort := strconv.FormatInt(cfg.PprofPort, 10)
+	/*
+		strPPPort := strconv.FormatInt(cfg.PprofPort, 10)
 
-	var tlsConf *tls.Config
-	if cfg.Crt != "" && cfg.Key != "" {
-		crt, err := tls.LoadX509KeyPair(cfg.Crt, cfg.Key)
-		if err != nil {
-			return err
+		var tlsConf *tls.Config
+		if cfg.Crt != "" && cfg.Key != "" {
+			crt, err := tls.LoadX509KeyPair(cfg.Crt, cfg.Key)
+			if err != nil {
+				return err
+			}
+
+			tlsConf = &tls.Config{
+				Certificates: []tls.Certificate{crt},
+			}
 		}
 
-		tlsConf = &tls.Config{
-			Certificates: []tls.Certificate{crt},
+		/*
+			sockR := http.NewServeMux()
+			sockR.HandleFunc("/", handleConnections)
+
+			socServer := &http.Server{
+				Addr:      ":" + strPort,
+				TLSConfig: tlsConf,
+				Handler:   sockR,
+			}
+
+		r := http.NewServeMux()
+		// Регистрация pprof-обработчиков
+		r.HandleFunc("/debug/pprof/", pprof.Index)
+		r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+		r.HandleFunc("/debug/pprof/profile", pprof.Profile)
+		r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+		r.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+		pprofServer := &http.Server{
+			Addr:      ":" + strPPPort,
+			TLSConfig: tlsConf,
+			Handler:   r,
+		}
+
+		defer func() {
+			//socServer.Close()
+			pprofServer.Close()
+		}()
+
+		errChan := make(chan error)
+
+		go func() {
+			if err := pprofServer.ListenAndServe(); err != nil {
+				errChan <- err
+			}
+		}()
+	*/
+	log.Println("http server started on " + strPort)
+
+	http.HandleFunc("/hello", handleConnections)
+	if cfg.Crt != "" && cfg.Key != "" {
+		if err := http.ListenAndServeTLS(":"+strPort, cfg.Crt, cfg.Key, nil); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if err := http.ListenAndServe(":"+strPort, nil); err != nil {
+			log.Fatal(err)
 		}
 	}
+
+	//log.Println("pprof http server started on " + strPPPort)
+	//sigChan := make(chan os.Signal, 1)
+	//signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
 
 	/*
-		sockR := http.NewServeMux()
-		sockR.HandleFunc("/", handleConnections)
-
-		socServer := &http.Server{
-			Addr:      ":" + strPort,
-			TLSConfig: tlsConf,
-			Handler:   sockR,
+		select {
+		case <-sigChan:
+			return nil
+		case e := <-errChan:
+			return e
 		}
 	*/
-
-	r := http.NewServeMux()
-	// Регистрация pprof-обработчиков
-	r.HandleFunc("/debug/pprof/", pprof.Index)
-	r.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
-	r.HandleFunc("/debug/pprof/profile", pprof.Profile)
-	r.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
-	r.HandleFunc("/debug/pprof/trace", pprof.Trace)
-
-	pprofServer := &http.Server{
-		Addr:      ":" + strPPPort,
-		TLSConfig: tlsConf,
-		Handler:   r,
-	}
-
-	defer func() {
-		//socServer.Close()
-		pprofServer.Close()
-	}()
-
-	errChan := make(chan error)
-
-	go func() {
-		if err := pprofServer.ListenAndServe(); err != nil {
-			errChan <- err
-		}
-	}()
-
-	go func() {
-		http.HandleFunc("/hello", handleConnections)
-		if cfg.Crt != "" && cfg.Key != "" {
-			if err := http.ListenAndServeTLS(":"+strPort, cfg.Crt, cfg.Key, nil); err != nil {
-				errChan <- err
-			}
-		} else {
-			if err := http.ListenAndServe(":"+strPort, nil); err != nil {
-				errChan <- err
-			}
-		}
-	}()
-
-	log.Println("http server started on " + strPort)
-	log.Println("pprof http server started on " + strPPPort)
-
-	sigChan := make(chan os.Signal, 1)
-	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
-
-	select {
-	case <-sigChan:
-		return nil
-	case e := <-errChan:
-		return e
-	}
+	return nil
 }
 
 func handleConnections(w http.ResponseWriter, r *http.Request) {
